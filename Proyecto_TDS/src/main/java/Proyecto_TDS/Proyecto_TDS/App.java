97a67f8c8ce95b68443956dab206e277d39ceef8
PacoPalazon <Pacofortunero@hotmail.com>
2025-09-22 10:06:15 +0200

    Proyecto Maven creado
    Hemos creado el proyecto Maven en clase de prácticas. Hemos elegido org.maven con el modo quick start

---
+++ Proyecto_TDS/src/main/java/Proyecto_TDS/Proyecto_TDS/App.java
@@ -0,0 +1,10 @@
+package Proyecto_TDS.Proyecto_TDS;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Proyecto_TDS/src/test/java/Proyecto_TDS/Proyecto_TDS/AppTest.java
@@ -0,0 +1,19 @@
+package Proyecto_TDS.Proyecto_TDS;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
